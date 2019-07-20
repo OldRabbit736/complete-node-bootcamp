acb4f07a28d2c263c07e01ad98807ef2da7df2bf
OldRabbit736 <36852907+OldRabbit736@users.noreply.github.com>
2019-07-20 21:26:44 +0900

    '13. Building a (Very) Simple API' done

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -4,6 +4,10 @@ const url = require('url');
 
 /////////////////////////////////
 // SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+// __dirname => place where the current file(index.js) is located
+const dataObject = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
@@ -12,10 +16,10 @@ const server = http.createServer((req, res) => {
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT');
     } else if (pathName === '/api') {
-
-        fs.readFile(`${__dirname}/dev-data/data.json`);
-        
-        res.end('API');
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
+        res.end(data);
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
